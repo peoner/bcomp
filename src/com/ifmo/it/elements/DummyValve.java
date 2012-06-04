cc6149316ef6f853ccca9ef2b961124ffa9976b5
MATPOCKuH <MATPOCKuH@0c4f0b21-4c53-9ef7-b6dc-97afc79b4082>
2012-06-04 19:25:33 +0000

    Some optimizations in ControlUnit

--- src/com/ifmo/it/elements/DummyValve.java
+++ src/com/ifmo/it/elements/DummyValve.java
@@ -6,13 +6,24 @@ package com.ifmo.it.elements;
 
 public class DummyValve extends DataHandler
 {
+	private int defvalue;
+
+	public DummyValve(int value, int width, DataSource ... ctrls)
+	{
+		super(width, ctrls);
+
+		defvalue = value & mask;
+	}
+
 	public DummyValve(DataSource ... ctrls)
 	{
 		super(1, ctrls);
+
+		defvalue = 1;
 	}
 
 	public void setValue(int ctrl)
 	{
-		super.setValue(1);
+		super.setValue(defvalue);
 	}
 }
