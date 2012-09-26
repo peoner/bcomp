8a8ff1332e65251a6eaae5d87c0cdd0744844324
MATPOCKuH <MATPOCKuH@0c4f0b21-4c53-9ef7-b6dc-97afc79b4082>
2012-09-26 13:20:38 +0000

    1. Added BusView 2. Removed Graphics2D functions

--- branches/bcomp/src/ru/ifmo/cs/bcomp/ui/components/ALUView.java
+++ branches/bcomp/src/ru/ifmo/cs/bcomp/ui/components/ALUView.java
@@ -6,7 +6,6 @@ package ru.ifmo.cs.bcomp.ui.components;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 import static ru.ifmo.cs.bcomp.ui.components.DisplayStyles.*;
@@ -16,38 +15,35 @@ import static ru.ifmo.cs.bcomp.ui.components.DisplayStyles.*;
  * @author Dmitry Afanasiev <KOT@MATPOCKuH.Ru>
  */
 public class ALUView extends JComponent {
-	private int x[];
-	private int y[];
+	private int xpoints[];
+	private int ypoints[];
 
 	public ALUView(int x, int y, int width, int height) {
 		int half = width / 2;
 		int offset = height / 3;
 		int soffset = offset / 3;
 
-		this.x = new int[] {
+		xpoints = new int[] {
 			0, half - soffset, half, half + soffset, width - 1, width - 1 - offset, offset
 		};
-		this.y = new int[] {
+		ypoints = new int[] {
 			0, 0, offset, 0, 0, height - 1, height - 1
 		};
 
 		JLabel title = new JLabel("АЛУ", JLabel.CENTER);
 		title.setFont(FONT_COURIER_BOLD_45);
-		title.setBounds(0, offset, width, height - offset);
+		title.setBounds(offset, offset, width - 2 * offset, height - offset);
 		add(title);
-		
+
 		setBounds(x, y, width, height);
 	}
 
 	@Override
 	public void paintComponent(Graphics g) {
-		Graphics2D rs = (Graphics2D) g;
-
-		rs.setPaint(COLOR_TITLE);
-        rs.fillPolygon(x, y, x.length);
-        //rs.setStroke(new BasicStroke(1.0f));
-        rs.setPaint(Color.BLACK);
-        rs.drawPolygon(x, y, x.length);
+		g.setColor(COLOR_TITLE);
+        g.fillPolygon(xpoints, ypoints, xpoints.length);
+        g.setColor(Color.BLACK);
+        g.drawPolygon(xpoints, ypoints, xpoints.length);
 
 	}
 }
--- branches/bcomp/src/ru/ifmo/cs/bcomp/ui/components/BasicView.java
+++ branches/bcomp/src/ru/ifmo/cs/bcomp/ui/components/BasicView.java
@@ -4,8 +4,10 @@
 
 package ru.ifmo.cs.bcomp.ui.components;
 
+import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
+import java.util.ArrayList;
+import java.util.EnumMap;
 import ru.ifmo.cs.bcomp.CPU;
 import ru.ifmo.cs.bcomp.ui.GUI;
 
@@ -14,21 +16,42 @@ import ru.ifmo.cs.bcomp.ui.GUI;
  * @author Dmitry Afanasiev <KOT@MATPOCKuH.Ru>
  */
 public class BasicView extends BCompPanel {
+	private enum Buses { DATA2ALU, INSTR2ALU, IP2ALU, KEY2ALU };
 	private GUI gui;
 	private CPU cpu;
 	private ComponentManager cmanager;
+	private ArrayList<BusView> openbuses = new ArrayList<BusView>();
+	private EnumMap<Buses, BusView> buses = new EnumMap<Buses, BusView>(Buses.class);
 
 	public BasicView(GUI gui) {
 		this.gui = gui;
 		this.cpu = gui.getCPU();
 		this.cmanager = gui.getComponentManager();
 
+		buses.put(Buses.DATA2ALU, new BusView(openbuses, new int[][] {
+			{550, 400},
+			{550, 450}
+		}));
+		buses.put(Buses.INSTR2ALU, new BusView(openbuses, new int[][] {
+			{650, 450},
+			{650, 400}
+		}));
+		buses.put(Buses.IP2ALU, new BusView(openbuses, new int[][] {
+			{580, 400},
+			{630, 400}
+		}));
+		buses.put(Buses.KEY2ALU, new BusView(openbuses, new int[][] {
+			{630, 450},
+			{580, 450}
+		}));
+
 		add(new ALUView(550, 200, 181, 90));
 	}
 
 	@Override
 	public void paintComponent(Graphics g) {
-        Graphics2D rs = (Graphics2D) g;
+		for (Buses bustype : Buses.values())
+			buses.get(bustype).draw(g, Color.GRAY);
 	}
 
 	@Override
@@ -58,12 +81,23 @@ public class BasicView extends BCompPanel {
 		cpu.addDestination(13, reg);
 		add(reg);
 
+		cpu.addDestination(1, buses.get(Buses.DATA2ALU));
+		cpu.addDestination(2, buses.get(Buses.INSTR2ALU));
+		cpu.addDestination(3, buses.get(Buses.IP2ALU));
+		cpu.addDestination(6, buses.get(Buses.KEY2ALU));
+
 		cmanager.panelActivate(this);
 	}
 
 	@Override
 	public void panelDeactivate() {
 		cpu.removeDestination(13, cmanager.getRegisterView(CPU.Regs.STATE));
+
+		cpu.removeDestination(1, buses.get(Buses.DATA2ALU));
+		cpu.removeDestination(2, buses.get(Buses.INSTR2ALU));
+		cpu.removeDestination(3, buses.get(Buses.IP2ALU));
+		cpu.removeDestination(6, buses.get(Buses.KEY2ALU));
+
 		cmanager.panelDeactivate();
 	}
 
@@ -77,9 +111,21 @@ public class BasicView extends BCompPanel {
 		return (InputRegisterView)cmanager.getRegisterView(CPU.Regs.KEY);
 	}
 
-	@Override
-	public void stepStart() { }
+	private void drawOpenBuses(Color color) {
+		Graphics g = getGraphics();
+
+		for (BusView bus : openbuses)
+			bus.draw(g, color);
+	}
 
 	@Override
-	public void stepFinish() { }
+	public void stepStart() {
+		drawOpenBuses(Color.GRAY);
+		openbuses.clear();
+	}
+
+	@Override
+	public void stepFinish() {
+		drawOpenBuses(Color.RED);
+	}
 }
