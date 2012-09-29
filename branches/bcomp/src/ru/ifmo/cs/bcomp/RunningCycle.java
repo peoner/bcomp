8dcb3440d2def17731f6b5a72403f889d7dee946
MATPOCKuH <MATPOCKuH@0c4f0b21-4c53-9ef7-b6dc-97afc79b4082>
2012-09-29 17:47:27 +0000

    Refactoring: enum ControlUnit.Cycle moved to RunningCycle

---
+++ branches/bcomp/src/ru/ifmo/cs/bcomp/RunningCycle.java
@@ -0,0 +1,32 @@
+/*
+ * $Id$
+ */
+
+package ru.ifmo.cs.bcomp;
+
+/**
+ *
+ * @author Dmitry Afanasiev <KOT@MATPOCKuH.Ru>
+ */
+public enum RunningCycle {
+	/**
+	 * Выборка команды
+	 */
+	INSTR_FETCH,
+	/**
+	 * Выборка адреса
+	 */
+	ADDR_FETCH,
+	/**
+	 * Исполнение
+	 */
+	EXECUTION,
+	/**
+	 * Прерывание
+	 */
+	INTERRUPT,
+	/**
+	 * Пультовая операция
+	 */
+	PANEL
+}
